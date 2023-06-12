caff5fa0ed0b10af6b18eae387662862874d9656
lavanya <lavanyabogavarapu22@gmail.com>
2023-06-12 10:29:44 +0000

    BooksArray Database using GET Method

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite = require("sqlite3");
+
+const app = express();
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server is running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
